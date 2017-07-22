6a7a2a4f000f7b157c81bbaece4a2f98d1b68c8e
Fliko <baxter.flw@gmail.com>
2017-07-22 15:31:40 -0500

    init plus some styling

---
+++ app.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function(e) {
+  let distance = window.scrollY;
+  let layers = document.querySelectorAll('[data-type="parallax"]');
+
+  for(let i = 0; i < layers.length; i++) {
+    let depth = layers[i].getAttribute('data-depth');
+    let move = (-distance * depth);
+
+    translate = 'translate3d(0, ' + move + 'px, 0)';
+    layers[i].style['-webkit-transform'] = translate;
+    layers[i].style['-moz-transform'] = translate;
+    layers[i].style['-ms-transform'] = translate;
+    layers[i].style['-o-transform'] = translate;
+    layers[i].style.transform = translate;
+  }
+});
