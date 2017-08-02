c307ff246f96a2f802938a5f6af110dd02a1bdb7
Fliko <baxter.flw@gmail.com>
2017-08-02 17:17:08 -0500

    Stop rerouting after sent email

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ function check_empty() {
     const email = document.getElementById('email').text;
     const message = document.getElementById('msg').text;
 
-    document.getElementById('form').submit(function() {
-      emailjs.send('gmail', 'template_Q95Ds4tu', {'from_name': name, 'from_mail': email, "to_name": 'Trace', 'message_html': message})
+    document.getElementById('form').addEventListener('click', function(e) {
+      e.preventDefault();
+      emailjs.send('gmail', templateKey, {'from_name': name, 'from_mail': email, "to_name": 'Trace', 'message_html': message})
               .then(
                 function(response) {
                   document.getElementById('pass').classList.toggle('visible');
